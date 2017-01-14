4a6fc826303b4655f7de5d8e3cefb6498a1d09be
Fred ST <frederic.soumare@gmail.com>
2017-01-14 08:05:06 +0100

    Fix README and demo compatibility with WP REST API v2.0

--- tests/dummy/app/routes/page.js
+++ tests/dummy/app/routes/page.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
 	model(params) {
 		// Query a single page by slug/name
 		return this.store.query('page', {
-			filter: {name: params.slug}
+			slug: params.page_slug
 		}).then(models => models.get('firstObject'));
 	}
 });
