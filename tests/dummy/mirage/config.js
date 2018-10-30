8daecb6dcc21ade8f4ca665d071a35f203bec0c5
Oliver Görtz <o.goertz@mmkh.de>
2018-10-30 17:51:42 +0100

    implement filtering feature into mirage config.js

--- tests/dummy/mirage/config.js
+++ tests/dummy/mirage/config.js
@@ -4,9 +4,34 @@ export default function () {
   this.urlPrefix = config.wordpressHost;
   this.namespace = '/wp-json/wp/v2';
 
-  this.resource('category');
-  this.resource('page');
-  this.resource('post');
-  this.resource('tag');
-  this.get('/media/:id', 'attachment');
+  this.get('/posts', ({posts}, request) => {
+    return filterRecordsByRequest(posts, request);
+  });
+
+  this.get('/pages', ({pages}, request) => {
+    return filterRecordsByRequest(pages, request);
+  });
+
+  this.get('/media', ({attachments}, request) => {
+    return filterRecordsByRequest(attachments, request);
+  });
+
+  this.get('/tags', ({tags}, request) => {
+    return filterRecordsByRequest(tags, request);
+  });
+
+  this.get('/categories', ({tags}, request) => {
+    return filterRecordsByRequest(tags, request);
+  });
+
+  this.get('/tags/:id');
+  this.get('/categories/:id');
+
+  const filterRecordsByRequest = function (records, request) {
+    const per_page = request.queryParams.per_page ? request.queryParams.per_page : 10;
+    const page = request.queryParams.page ? request.queryParams.page : 1;
+    let where_params = {};
+    if (request.queryParams.slug) where_params.slug = request.queryParams.slug;
+    return records.where(where_params).slice((per_page * page) - per_page, per_page * page);
+  }
 }
