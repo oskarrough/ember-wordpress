55cee38c2c9bfb5b88165cc522c50146807cb3aa
Oliver Görtz <o.goertz@mmkh.de>
2018-10-30 15:58:45 +0100

    implement mirage serializer to mock wordpress api

--- tests/dummy/mirage/serializers/application.js
+++ tests/dummy/mirage/serializers/application.js
@@ -1,4 +1,17 @@
-import { JSONAPISerializer } from 'ember-cli-mirage';
+import { RestSerializer } from 'ember-cli-mirage';
 
-export default JSONAPISerializer.extend({
+export default RestSerializer.extend({
+  serialize(object, request) {
+    let json = RestSerializer.prototype.serialize.call(this, object, request);
+    return json[Object.keys(json)[0]];
+  },
+
+  keyForForeignKey(relationshipName) {
+    if (relationshipName === 'wp:featuredmedia') return relationshipName;
+    return RestSerializer.prototype.keyForForeignKey.apply(this, arguments);
+  },
+
+  keyForAttribute(attr){
+    return attr;
+  }
 });
