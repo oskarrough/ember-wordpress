ba256c2d544a9903abb216159a811ae4df82bc4e
Francesco Novy <francesco@fabscale.com>
2021-06-07 13:11:13 +0200

    Use `htmlSafe` for rendered transform

--- addon/transforms/rendered.js
+++ addon/transforms/rendered.js
@@ -1,12 +1,13 @@
 import Transform from '@ember-data/serializer/transform';
+import { htmlSafe } from '@ember/template';
 
 export default class RenderedTransform extends Transform {
   deserialize(serialized) {
     // Depending on Wordpress version, this will exist. Or not.
     if (serialized.rendered) {
-      return serialized.rendered;
+      return htmlSafe(serialized.rendered);
     }
-    return serialized;
+    return htmlSafe(serialized);
   }
 
   serialize(deserialized) {
